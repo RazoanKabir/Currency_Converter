da375c55e20b39a6aceceb0b914e8a0fc1963a89
razoankabir <razoan@sheba.xyz>
2022-03-17 03:41:37 +0600

    unnecessary variables removed and updated packages, added comments

--- app/src/main/java/com/razoan/currencyconverter/util/DataConfig.kt
+++ app/src/main/java/com/razoan/currencyconverter/util/DataConfig.kt
@@ -6,6 +6,6 @@ import com.razoan.currencyconverter.util.DataConfig.Companion.BASE_URL
 class DataConfig {
     companion object {
         val BASE_URL: String = "http://api.currencylayer.com/"
-        val ACCESS_KEY: String = "d54171e09ecbe6ad23a259773f6c2927"
+        val ACCESS_KEY: String = "d54171e09ecbe6ad23a259773f6c2927" // for free trial, key will be serve for only 7 days
     }
 }
